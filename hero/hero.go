d79234b98d3239116d38a2482e9dfbcc870e3f40
George Kuznetsov <george.kuznetsov@piano.io>
2017-11-19 15:08:51 +0400

    init weapons

--- hero/hero.go
+++ hero/hero.go
@@ -100,3 +100,39 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "weaponfactory"
+  type: "factory"
+  data: "prototype: \"/weapon/weapon.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "weaponlevelfactory"
+  type: "factory"
+  data: "prototype: \"/weapon/weapon_level.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}

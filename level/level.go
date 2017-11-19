951e477195c14e7192e6e7952bc56ba644f28c1b
George Kuznetsov <george.kuznetsov@piano.io>
2017-11-19 15:08:56 +0400

    init weapons

---
+++ level/level.go
@@ -0,0 +1,51 @@
+components {
+  id: "level"
+  component: "/level/level.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "weaponfactory"
+  type: "factory"
+  data: "prototype: \"/weapon/weapon.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "weaponlevelfactory"
+  type: "factory"
+  data: "prototype: \"/weapon/weapon_level.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ weapon/weapon_level.go
@@ -0,0 +1,55 @@
+components {
+  id: "script"
+  component: "/weapon/weapon_level.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sword"
+  type: "sprite"
+  data: "tile_set: \"/weapon/weapon.atlas\"\n"
+  "default_animation: \"sword\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sword2"
+  type: "sprite"
+  data: "tile_set: \"/weapon/weapon.atlas\"\n"
+  "default_animation: \"sword2\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}

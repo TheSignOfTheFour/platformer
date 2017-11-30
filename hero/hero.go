09029a24e4238ef972d95469bf556d10c89197bb
George Kuznetsov <george.kuznetsov@piano.io>
2017-11-30 19:02:49 +0400

    add walking to troll

--- hero/hero.go
+++ hero/hero.go
@@ -14,7 +14,7 @@ components {
   }
 }
 embedded_components {
-  id: "collisionobject"
+  id: "collisionobject_right"
   type: "collisionobject"
   data: "collision_shape: \"\"\n"
   "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
@@ -25,6 +25,7 @@ embedded_components {
   "mask: \"geometry\"\n"
   "mask: \"bestiary\"\n"
   "mask: \"bestiary_attack_range\"\n"
+  "mask: \"bestiary_watch_range\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
@@ -125,6 +126,56 @@ embedded_components {
   type: "factory"
   data: "prototype: \"/weapon/weapon_level.go\"\n"
   "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "collisionobject_left"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"hero\"\n"
+  "mask: \"geometry\"\n"
+  "mask: \"bestiary\"\n"
+  "mask: \"bestiary_attack_range\"\n"
+  "mask: \"bestiary_watch_range\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: -9.640521\n"
+  "      y: 28.211432\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 19.835552\n"
+  "  data: 26.133411\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
   ""
   position {
     x: 0.0
